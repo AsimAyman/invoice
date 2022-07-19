c074e4344f5af4535d1de8c561c55f8aebdac0fc
Assem <Assemayman136@gmail.com>
2022-07-19 16:48:40 +0200

    f

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject1;
+
+import javax.swing.JFrame;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+      JFrame frame= new MyFrame();
+    }
+}
